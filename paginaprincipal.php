6a836f753b05e6096dacbdf416dedf0f12f788fd
geravs <escobaraguilarnagai@gmail.com>
2026-06-12 15:42:28 -0400

    herencia nav

--- paginaprincipal.php
+++ paginaprincipal.php
@@ -64,87 +64,7 @@
             font-size: 1.25rem;
         }
        
-        #a{
-            grid-area: cuerpo1;
-            padding-top: 80px;
-            padding-bottom: 80px;
-            display: flex;
-            gap: 30px;
-            justify-content: center;
-            flex-wrap: nowrap;
-        }
-
-        #barra{
-            position: fixed;
-            width: 1300px;
-            height: 65px;
-            margin-left: 280px;
-            margin-right: 100px;
-            margin-top: 20px;
-            background:none;
-            border-radius: 50px;
-            display:flex;
-            justify-content: space-between;
-            flex-wrap: nowrap;
-        }      
        
-        #barra.desliza{
-            background-color: black;
-            transition: 1s;
-        }
-
-        #orga{
-            position: relative;
-            left: 15px;
-            bottom: 8px;
-        }
-        a{
-            display: flex;
-            text-decoration: none;
-            color: white;
-        }
-
-
-        #links{
-            display: flex;
-            flex-direction: row;
-        }
-       
-        #link1{
-            margin-right: 10px;
-        }
-        #link2{
-            margin-right: 20px;
-            margin-left: 20px;
-        }
-       
-        #link3{
-            margin-left: 10px;
-        }
-        
-        #linkprodu{
-            background-color: rgba(172, 255, 47, 0.705);
-            padding: 13px;
-            padding-left: 20px;
-            border-radius: 50px;
-            position: relative;
-            top: 7px;
-            right: 8px;
-            font-size: 20px;
-        }
-       
-        #descu{
-            background-color: rgba(172, 255, 47, 0.705);
-            padding: 13px;
-            padding-left: 20px;
-            border-radius: 50px;
-        }
-       
-        #descu2{
-            position: relative;
-            bottom: 11px;
-            right: 8px;
-        }
                                                                      
         .productos{
             width: 400px;
@@ -323,122 +243,22 @@
     height: auto;
   }
 }
-.menu-desplegable{
-    position: relative;
-}
 
-.submenu{
-    display: none;
-    position: absolute;
-    top: 100%;
-    left: 0;
-    min-width: 220px;
-    background: black;
-    border-radius: 15px;
-    overflow: visible;
-    z-index: 1000;
-}
-
-.menu-desplegable:hover .submenu{
-    display: block;
-}
-
-.submenu-item{
-    position: relative;
-    display: block;
-    padding: 20px;
-    color: white;
-    text-decoration: none;
-    background: black;
-    border-radius: 30px;
-    background-color: rgba(172,255,47,0.7);
-}
-
-.submenu a:hover{
-    background: rgba(172,255,47,0.7);
-    color: black;
-}
-
-
-.submenu-lateral{
-    display: none;
-    position: absolute;
-    top: 0;
-    left: 100%;
-    min-width: 220px;
-    background: black;
-    border-radius: 15px;
-    overflow: hidden;
-}
-
-.submenu-item:hover .submenu-lateral{
-    display: block;
-}
 
     </style>
 </head>
 <body>
-    <script>
-        document.addEventListener('scroll', () => {
-        const header = document.querySelector('header');
-        const scrollY = window.scrollY;
-        const newSize = Math.min(120, 100 + scrollY / 15);
-        header.style.backgroundSize = `${newSize}%`;
-        });
-        window.addEventListener('scroll', () => {
-        const barra = document.getElementById('barra');
-        if (window.scrollY > 50) {
-        barra.classList.add('desliza');  // Aplica clase que reduce el menú
-        } else {
-        barra.classList.remove('desliza'); // Quita clase cuando vuelve arriba
-        }
-        });
-    </script>  
-
-    <script>
-        document.addEventListener('scroll', () => {
-        const header = document.querySelector('header');
-        const scrollY = window.scrollY;
-        const newSize = Math.min(120, 100 + scrollY / 15);
-        header.style.backgroundSize = `${newSize}%`;
-        });
-        </script>  
-
+    <nav>
+         <?php 
+        include("nav.php");
+        ?>
+    </nav>
+    
     <header>
         <div class="fondo"></div>
         <div class="texto-header">
             
     </header>
-    <center>
-    <nav id="barra">
-    <div><a href="" id="orga"><h1>OrganicZone</h1></a></div>
-    <section id="links">
-    <a href="contacto.html" id="link2"><h3>Contacto</h3></a>
-    <a href="sobreNosotros.html" id="link3"><h3>Sobre Nosotros</h3></a>
-    </section>
-    <section> <strong><a href="pedidos.php" id="linkprodu">Pide aquí</a></section></strong>
-    <section><a href="Clientes/login.php" id="descu2"><h3 id="descu"> Iniciar Sesion</h3></a></section>
-    <div class="menu-desplegable">
-    <a href="#"><h3>Menú ▼</h3></a>
-
-    <div class="submenu">
-
-        <div class="submenu-item">
-            <a href="#">🍔 Hamburguesas</a>
-        </div>
-
-        <div class="submenu-item">
-            <a href="#">🥤 Refrescos</a>
-        </div>
-
-        <div class="submenu-item">
-            <a href="#">🥛 Mi Leche</a>
-        </div>
-
-    </div>
-</div>
-    </nav>
-    </center>
 
     <section id="b" >
         <div id="cinco" >
