4b6e44dbc50a22e34e6f8e497269f21965c814ad
SebastianReynoldsCastano <escobaraguilarnagai@gmail.com>
2026-06-19 12:29:27 -0400

    cambios

--- Pedidos/leercarrito.php
+++ Pedidos/leercarrito.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servidor, $usuario, $contrasena, $bd);
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
-$idPedido = $_GET['idPedido'];
+$idPedido = $_GET['pedidos_id'];
 
 $sql = "SELECT * FROM productos";
 
