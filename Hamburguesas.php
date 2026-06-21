d34751a8fef890b3f383190927390a8cfd2b9160
fabricioC-U <fabriciocalerourey@gmail.com>
2026-06-20 22:41:27 -0400

    cambios

--- Hamburguesas.php
+++ Hamburguesas.php
@@ -5,16 +5,20 @@ body{
     box-sizing:border-box;
     font-family:Arial, sans-serif;
 }
+.menu{
+    padding:40px;
+}
 
 .menu h1{
+    font-size:80px;
     color:#0c8d2f;
-    font-size:70px;
     margin-bottom:30px;
     font-weight:bold;
 }
 
 .productos{
     display:flex;
+    justify-content:center;
     gap:25px;
     flex-wrap:wrap;
 }
@@ -37,16 +41,6 @@ body{
     font-weight:bold;
 }
 
-.carta{
-    position:relative;
-}
-
-.carta h2{
-    position:absolute;
-    top:20px;
-    left:20px;
-}
-
 .precio{
     display:flex;
     justify-content:space-between;
@@ -60,7 +54,7 @@ body{
     color:#0c8d2f;
 }
 
-button{
+.precio button{
     background:#0c8d2f;
     color:white;
     border:none;
@@ -71,7 +65,7 @@ button{
 }
 
 button:hover{
-    opacity:.9;
+    opacity:.8;
 }
 </style>
 <body>
