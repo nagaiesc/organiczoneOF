8684e0ebe404d23baf5d6f182f7820dce4e04769
Jhanael Villacorta Altamirano <jhanaelvillacortaaltamirano@gmail.com>
2026-07-31 12:20:33 -0400

    ventas

---
+++ Ventas/ventas.php
@@ -0,0 +1,26 @@
+<?php
+$servidor = "localhost";
+$nombre = "root";
+$contraseña = "";
+$BDnombre = "organiczoneBD";
+ $conn = new mysqli($servidor, $nombre, $contraseña, $BDnombre);
+  if($conn->connect_error) {
+    die ("conexion fallida" . $conn->connect_error);
+  }
+  $id = $_POST['id'];
+  $estado = $_POST['estado'];
+  $metodo = $_POST['metodo'];
+  $costototal = $_POST['costo'];
+  $pedidos_id = $_POST['stock'];
+  
+  $sql = "INSERT INTO ventas (id, estado, metodo, costototal, pedidos_id)
+  VALUES ('$id', '$estado', '$metodo', '$costototal', '$pedidos_id')";
+  if($conn->query($sql) === TRUE) {
+    echo "Nuevo venta creado correctamente";
+    header("Location: leerventas.php?");
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+?>
