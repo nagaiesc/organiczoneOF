e4e5e179d9c0f7668e5c5bfc989d09dbf7d4850a
Sebastian Reynolds Castaño <sebitas143uwu@gmail.com>
2026-08-24 13:04:59 -0400

    Update vistacliente.php

--- Cliente/vistacliente.php
+++ Cliente/vistacliente.php
@@ -80,7 +80,6 @@ function obtenerImagenProducto(int $id): string
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Organic Zone | Cliente</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
